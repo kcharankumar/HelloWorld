bd1f2f9bd8dc6b8a3e3fb2c77077b499f6c1d545
Charan K <kcharankumar@gmail.com>
2022-10-26 13:30:07 +0530

    New changes to HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ class HelloWorld{
 
   public static void main (String args[]){
 
-    System.out.println ("Hello World...Build through Jenkins...Welcome to Jenkins");
+    System.out.println ("Running the build from jenkins...From Charan...");
 
 }
 
