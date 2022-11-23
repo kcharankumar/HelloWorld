3114bc2756a30be3118c8cbf1b0dc134e0d8f6f6
K.Charan Kumar <kcharankumar@gmail.com>
2022-11-23 12:25:11 +0530

    Text is modified in HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ class HelloWorld{
 
   public static void main (String args[]){
 
-    System.out.println ("Good morning from Jenins... Running the build from Jenkins... By Charan Kumar");
+    System.out.println ("Running build from jenkins...");
 
 }
 
