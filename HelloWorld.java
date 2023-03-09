eab6de947d7c9cb8e9cc9cad71eebc38c942a111
Charan Kumar <kcharankumar@gmail.com>
2023-03-09 13:13:05 +0530

    Text to be displayed is changed...

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ class HelloWorld{
 
   public static void main (String args[]){
 
-    System.out.println ("Jenkins made the build and executed it successfully...");
+    System.out.println ("Hello from the land of Jenkins...");
 
 }
 
