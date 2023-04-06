eb30975692d21796ffd2a4260cc5c00e37054696
Charan Kumar.K <kcharankumar@gmail.com>
2023-04-06 11:11:32 +0530

    New text is added in HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ class HelloWorld{
 
   public static void main (String args[]){
 
-    System.out.println ("Hello from the land of Jenkins...");
+    System.out.println ("Welcome to Jenkins...compiling java (GITHUB) from jenkins..");
 
 }
 
