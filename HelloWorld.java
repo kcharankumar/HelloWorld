f4dbda084ed71e4da0dd64b8a9ac1898646d8d3a
Charan Kumar.K <kcharankumar@gmail.com>
2023-05-09 12:24:05 +0530

    Text is modified

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ class HelloWorld{
 
   public static void main (String args[]){
 
-    System.out.println ("Building HelloWorld.java from jenkins....");
+    System.out.println ("We looked at dev-ops tools GIT, MAVEN, JUnit and Jenkins...Docker is pending...");
 
 }
 
