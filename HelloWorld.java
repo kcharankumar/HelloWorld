7c23fee6274e07f55fe92db4b7b7e98baeaaef3e
Charan Kumar.K <kcharankumar@gmail.com>
2023-05-09 12:22:45 +0530

    Error correction done in HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ class HelloWorld{
 
   public static void main (String args[]){
 
-    System.out.println ("Building HelloWorld.java from jenkins....);
+    System.out.println ("Building HelloWorld.java from jenkins....");
 
 }
 
