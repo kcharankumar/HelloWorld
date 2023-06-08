5edff5cacd943e11a571cbd6facebac45502c14d
K.Charan Kumar <kcharankumar@gmail.com>
2023-06-08 13:32:07 +0530

    Text to display is modified

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ class HelloWorld{
 
   public static void main (String args[]){
 
-    System.out.println ("We are building git java project from jenkins...");
+    System.out.println ("Jenkins is a powerful tool to make the builds and to release it to the systems...");
 
 }
 
