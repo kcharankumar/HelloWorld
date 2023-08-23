520357968a820209a118731c451a3008a561d0ed
K.Charan Kumar <kcharankumar@gmail.com>
2023-08-23 13:34:26 +0530

    Text to display is modified again

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ class HelloWorld{
 
   public static void main (String args[]){
 
-    System.out.println ("Welcome to all from the JENKINS...");
+    System.out.println ("Jenkins is an amazing tool for building...");
 
 }
 
