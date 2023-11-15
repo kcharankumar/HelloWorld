a8e286ecadf7309ecaa0274547fa90afe650b272
K.Charan Kumar <kcharankumar@gmail.com>
2023-11-15 13:20:27 +0530

    Text to display is modified

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ class HelloWorld{
 
   public static void main (String args[]){
 
-    System.out.println ("Hello, Welcome to Jenkins.");
+    System.out.println ("Jenkins is a great build tool...It is a best CI/CD tool...");
 
 }
 
