0ce6d9d71b80544ebb22fc35557a6a7ad36579ac
K.Charan <kcharankumar@gmail.com>
2023-12-14 12:42:47 +0530

    Text to display is changed

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ class HelloWorld{
 
   public static void main (String args[]){
 
-    System.out.println ("Jenkins is the best tool for making a java build...");
+    System.out.println ("Jenkins is the best tool for making a java build...test");
 
 }
 
