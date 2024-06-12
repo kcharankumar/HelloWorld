eff0099223052039e70b070014f1f7f8cdaa402f
K.Charan <kcharankumar@gmail.com>
2024-06-12 17:40:12 +0530

    New text is added

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ class HelloWorld{
 
   public static void main (String args[]){
 
-    System.out.println ("Hello, Welcome to the land of Jenkins...");
+    System.out.println ("Hello, I was watching netflix...");
 
 }
 
