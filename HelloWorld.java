24c0f2c01b4c63e502d99cbed9f8813f18b4bee5
K.Charan Kumar <kcharankumar@gmail.com>
2026-01-09 16:42:59 +0530

    HelloWorld.java is modified

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ class HelloWorld{
 
   public static void main (String args[]){
 
-    System.out.println ("Hello, Surya is buttering u sir");
+    System.out.println ("Hello,how are you...");
 
 }
 
